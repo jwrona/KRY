355295ccd007c7f6a5bec87adef806a385334333
Jan Wrona <xwrona00@stud.fit.vutbr.cz>
2015-04-20 00:11:17 +0200

    2proj: solved and tested, add task assignment

--- 2proj/src/xwrona00.py
+++ 2proj/src/xwrona00.py
@@ -1,63 +1,112 @@
-################################################################################
-#                   Kostra k 2. projektu do predmetu KRY                       #
-################################################################################
-import sys
-from OracleModule import paddingOracle
-#from OracleModule import genNewKey
-#from OracleModule import setKey
-#from OracleModule import encrypt
+#!/usr/bin/env python2
+#author: Jan Wrona
+#email: xwrona00@stud.fit.vutbr.cz
 
-'''
-Utok budete provadet na funkci paddingOracle():
-    
-paddingOracle(ciphertext):
-  Funkce "zjisti" zda je zasifrovany plaintext korektne zarovnan podle PKCS#7
-  a vrati tuto informaci v podobe True/False.
-  Parametr ciphertext je retezec zasifrovaneho textu prevedeny do hexa formatu!
+import sys, os
+from binascii import hexlify, unhexlify
+from OracleModule import paddingOracle, genNewKey, setKey, encrypt
+
+block_len = 16 #in words
+word_len = 8 #in bits
+
+def last_word_oracle(block):
+    '''Return one or more last words of C^-1(block).
+
+    Usually only last block of C^-1(block) is returned. May return more
+    blocks if we are lucky and generated random numbers causes more blocks of
+    valid PKCS#7 padding on plaintext.
+    Arguments:
+    block -- block to decrypt (binary, not hexadecimal)
+    '''
+    rand_block = bytearray(os.urandom(block_len))
+
+    #Try all combinations of last word and ask oracle.
+    for i in range(2 ** word_len):
+        rand_block[-1] ^= i #alter
+        if paddingOracle(hexlify(rand_block + block)):
+            break
+        else:
+            rand_block[-1] ^= i #revert
+
+    #Check if one or more words were decrypted by gradually altering words from
+    #left to right. When oracle returns false, altered word represents position
+    #of the leftmost decrypted word of C^-1(block).
+    for i in range(block_len):
+        rand_block[i] ^= 1 #alter
+        oracle_res = paddingOracle(hexlify(rand_block + block))
+        if not oracle_res:
+            rand_block[i] ^= 1 #revert
+            return bytearray([word ^ (block_len - i) for word in rand_block[i:]])
 
 
+def next_word_oracle(block, known_suffix):
+    '''Return next word of C^-1(block).
 
-Pro jistotu upozorneni:
-  Nezapomente, ze zasifrovany text je v rezimu "CBC s nahodnym IV" ve formatu:
-      IV | CT
+    Initial known_suffix is obtained by call to last_word_oracle(). Next word
+    here means leftmost word of C^-1(block) not contained in known_suffix. If
+    all words of C^-1(block) are contained in known_suffix, None is returned.
+    Arguments:
+    block -- block do decrypt (binary, not hexadecimal)
+    known_suffix -- allready decrypted words from block
+    '''
+    if len(block) == len(known_suffix):
+        return #nothing to decrypt
 
-  IV - inicializacni vektor (16 bajtu)
-  |  - kontatenace
-  CT - zasifrovany text rezimem CBC (nasobek 16 bajtu)
+    new_padd_val = len(known_suffix) + 1 #new padding value == new padding length
+    next_word_index = block_len - new_padd_val
+    rand_block = bytearray(os.urandom(next_word_index + 1)) #random prefix
+    rand_block += bytearray([i ^ new_padd_val for i in known_suffix])
+
+    #Try all combinations of next word and ask oracle.
+    for i in range(2 ** word_len):
+        rand_block[next_word_index] ^= i #alter
+        if paddingOracle(hexlify(rand_block + block)):
+            return bytearray([rand_block[next_word_index] ^ new_padd_val])
+        else:
+            rand_block[next_word_index] ^= i #revert
 
 
+def decode_ciphertext(ciphertext):
+    '''Return decoded ciphertext as string.
 
-Pro testovani muzete pouzit funkce genNewKey(), setKey(key) a encrypt(plaintext).
----------------------
-genNewKey():
-  Provede vygenerovani noveho klice, ktery zaroven nastavi jako aktualni sifrovaci
-  klic pro padding orakulum. Rovnez vrati vygenerovany klic (ascii, nikoli hexa).
+    All data are internally handled as bytearrays.
+    Arguments:
+    ciphertext -- IV | ciphertext (hexadecimal representation)
+    '''
+    bin_c = bytearray(unhexlify(ciphertext))
 
-setKey(key):
-  Provede nastaveni sifrovaciho klice pro padding orakulum. Argument key ocekava
-  sifrovaci klic v ascii, nikoli jako hexa retezec.
-  
-encrypt(plaintext):
-  Provede zarovnani PKCS#7 ascii plaintextu a nasledne jeho zasifrovani 
-  s vyuzitim aktualne nastaveneho sifrovaciho klice, ktery sdili s padding 
-  orakulem. Sifrovani probiha algoritmem AES-CBC (128b varianta). 
-'''
+    #argument validity checks
+    if len(bin_c) == 0 or len(bin_c) % block_len != 0:
+        print 'Bad ciphertext length or no ciphertext at all.'
+        return
+    elif len(bin_c) / block_len < 2:
+        print 'Missing ciphertext, only IV present.'
+        return
 
-def decodeCiphertext(ciphertext):
-    # zde provedte utok CBC Padding Oracle
-    answer = paddingOracle(ciphertext)
-    plaintextWithoutPadding = "Plaintext by mel by vypsan bez pripadneho zarovnani!"
-    
-    return plaintextWithoutPadding
+    #cut ciphertext into IV and individual blocks
+    bin_block_list = [bin_c[i:i + block_len] for i in range(0, len(bin_c), block_len)]
 
-if __name__ == "__main__":
+    plain_int = []
+    #Decode each block separately and store results in plain_int list.
+    for block_idx, bin_block in enumerate(bin_block_list[1:]): #first is IV
+        known_suffix = last_word_oracle(bin_block)
+        while len(known_suffix) != block_len:
+            known_suffix = next_word_oracle(bin_block, known_suffix) + known_suffix
+
+        #On each decrypted word perform XOR with corresponding word of the previous
+        #block (IV in case of first block) to decode C^-1(block) into plaintext.
+        for word_idx, word in enumerate(known_suffix):
+            plain_int.append(bin_block_list[block_idx][word_idx] ^ word)
+ 
+    #Strip PKCS#7 padding and return plaintext in string representation.
+    return ''.join(chr(i) for i in plain_int[:-plain_int[-1]])
+
+
+if __name__ == '__main__':
     if len(sys.argv) > 1:
         ciphertext = sys.argv[1]
     else:
-        ciphertext = "fa485ab028cb239a39a9e52df1ebf4c30911b25d73f8906cc45b6bf87f7a693f47609094ccca42050ad609bb3cf979ac"
+        ciphertext = 'fa485ab028cb239a39a9e52df1ebf4c30911b25d73f8906cc45b6bf87f7a693f47609094ccca42050ad609bb3cf979ac'
 
-    # vypis desifrovaneho textu provedte nasledujicim zpusobem: 
-    print decodeCiphertext(ciphertext)
-    
+    print decode_ciphertext(ciphertext)
     sys.exit(0)
-    
